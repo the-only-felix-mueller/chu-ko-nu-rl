513b23ff1586a3d7934e5f2aec682ed9c0709cbb
the-only-felix-mueller <felix_im_internet@yahoo.de>
2020-09-12 16:18:30 +0200

    cleaned up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import { UI } from './ui/UI'
 
+/**
+ * This is the entry point of the whole program.
+ *
+ * An UI object is created, which in turn creates and manages a game world.
+ * This display of the UI object is added to the DOM and then the main game
+ * loop is started.
+ *
+ * The UI references the game model (`World`) like a player in a casino interacts
+ * with the game through the croupier. The world doesn't reference the UI.
+ */
 window.onload = async function (): Promise<void> {
   const ui = new UI()
   const uiPlaceholder = document.getElementById('ui-placeholder')
   const mainUI = ui.display.getContainer()
-  console.log(`${uiPlaceholder} --- ${mainUI}`);
-  // uiPlaceholder.parentNode.replaceChild(mainUI, uiPlaceholder)
   uiPlaceholder.appendChild(mainUI)
-  
+
   ui.mainLoop()
 }
